3479552e35f8521c81511c12a8666b5bd16c5e0a
Alain K <alain.kahwaji@gmail.com>
2015-05-05 20:17:00 -0700

    finish binarysearch

--- Binary Search/main.c
+++ Binary Search/main.c
@@ -8,8 +8,33 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+int binarySearch(int value, int numbers[], int minIndex, int maxIndex) {
+    if (minIndex > maxIndex) {
+        return -1;
+    } else {
+        int midpointIndex = ((maxIndex + minIndex)/2);
+        
+        if (numbers[midpointIndex] < value) {
+            return binarySearch(value, numbers, midpointIndex + 1, maxIndex);
+        } else if (numbers[midpointIndex] > value) {
+            return binarySearch(value, numbers, minIndex, midpointIndex - 1);
+        }else{
+            return midpointIndex;
+        }
+    }
+}
+
+int main()
+{
+    int an_array[] = {2, 4, 6, 8, 10};
+    
+    int arraySize = sizeof(an_array)/sizeof(int);
+    
+    int result = binarySearch(6, an_array, 0, arraySize);
+    printf("%d\n", result);
+    // binarySearch returns the index of the target value
     return 0;
 }
+
+
